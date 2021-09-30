ef7291377f40349a03ee3f447412ed2cdb003833
Kanika <ksharmaconnect3@gmail.com>
2021-09-29 23:11:15 -0400

    Modified file to include save_qualifying_loans function which sends qualifying loans to a csv file

--- loan_qualifier_app/app.py
+++ loan_qualifier_app/app.py
@@ -7,6 +7,7 @@ Example:
     $ python app.py
 """
 import sys
+import csv
 import fire
 import questionary
 from pathlib import Path
@@ -103,6 +104,7 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
 
 def save_qualifying_loans(qualifying_loans):
+    #print(qualifying_loans)
     """Saves the qualifying loans to a CSV file.
 
     Args:
@@ -111,6 +113,22 @@ def save_qualifying_loans(qualifying_loans):
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
 
+    header = ["Lender","Max Loan Amount","Max LTV","Max DTI","Min Credit Score","Interest Rate"]
+
+    csvpath = Path("my_output.csv")
+    with open(csvpath, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        # Write our header row first!
+        csvwriter.writerow(header)
+        #print(header)
+
+        # Then we can write the data rows
+        for bank_data in qualifying_loans:
+            #print(bank_data)
+            csvwriter.writerow(bank_data)
+            
+    
 
 def run():
     """The main function for running the script."""
@@ -129,6 +147,6 @@ def run():
     # Save qualifying loans
     save_qualifying_loans(qualifying_loans)
 
-
 if __name__ == "__main__":
     fire.Fire(run)
+
